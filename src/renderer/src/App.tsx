de34317af68a22b8255eed160fa590c5c677c099
kristersgcode <k.gutmanis@lvm.lv>
2024-02-18 14:57:55 +0200

    crete electron project

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,30 @@
+function App(): JSX.Element {
+  return (
+    <div
+      className="
+      flex
+      flex-col
+      items-center
+      justify-center
+      min-h-screen
+      bg-gray-100
+
+
+    "
+    >
+      <h1
+        className="
+        text-6xl
+        font-bold
+        text-center
+        mt-20
+        text-blue-500
+      "
+      >
+        Hello Electron!
+      </h1>
+    </div>
+  )
+}
+
+export default App
